a722b844fa2e6acb7b9a8926b5dba6c90071fd61
Dzikri Khasnudin <dzikri.khasnudin2@gmail.com>
2023-04-29 01:10:10 +0700

    replace vite with laravel mix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,9 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700,800,900&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}" defer></script>
     <!-- Styles -->
     @livewireStyles
 </head>
