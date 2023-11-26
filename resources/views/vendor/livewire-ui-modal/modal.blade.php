a8ceb4e254f24fe937575e895e5d16bb1babc6b5
dkhasnudin <dzikri.khasnudin2@gmail.com>
2023-11-26 21:11:07 +0700

    membuat margin sumbu y menjadi auto pada modal ui

--- resources/views/vendor/livewire-ui-modal/modal.blade.php
+++ resources/views/vendor/livewire-ui-modal/modal.blade.php
@@ -33,7 +33,7 @@
                 x-transition:leave="ease-in duration-200"
                 x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
                 x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" x-bind:class="modalWidth"
-                class="inline-block w-full align-middle bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle mb-20 sm:w-full"
+                class="inline-block w-full align-middle bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle my-auto sm:w-full"
                 id="modal-container">
                 @forelse($components as $id => $component)
                 <div x-show.immediate="activeComponent == '{{ $id }}'" x-ref="{{ $id }}" wire:key="{{ $id }}">
