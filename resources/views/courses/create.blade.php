27511855b4d9d9452b6c2aa7791ae3e8570c79a2
dkhasnudin <dzikri.khasnudin2@gmail.com>
2023-11-26 21:07:55 +0700

    menambahkan margin bottom pada modal ui

--- resources/views/courses/create.blade.php
+++ resources/views/courses/create.blade.php
@@ -7,8 +7,8 @@
             @csrf
             <div>
                 <x-label for="name" value="{{ __('Judul Kelas') }}" />
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')"
-                    autofocus required />
+                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" autofocus
+                    required />
             </div>
             <div class="mt-4">
                 <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -19,8 +19,7 @@
 
             </div>
             <div class="mt-4">
-                <label for="status"
-                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Status</label>
+                <label for="status" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Status</label>
                 <select id="status"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     name="status">
--- resources/views/vendor/livewire-ui-modal/modal.blade.php
+++ resources/views/vendor/livewire-ui-modal/modal.blade.php
@@ -1,20 +1,22 @@
 <div>
     @isset($jsPath)
-        <script>
-            {!! file_get_contents($jsPath) !!}
-        </script>
+    <script>
+        {!! file_get_contents($jsPath) !!}
+    </script>
     @endisset
     @isset($cssPath)
-        <style>
-            {!! file_get_contents($cssPath) !!}
-        </style>
+    <style>
+        {
+             ! ! file_get_contents($cssPath) ! !
+        }
+    </style>
     @endisset
 
     <div x-data="LivewireUIModal()" x-init="init()" x-on:close.stop="setShowPropertyTo(false)"
         x-on:keydown.escape.window="closeModalOnEscape()"
         x-on:keydown.tab.prevent="$event.shiftKey || nextFocusable().focus()"
-        x-on:keydown.shift.tab.prevent="prevFocusable().focus()" x-show="show" class="fixed inset-0 z-10 overflow-y-auto"
-        style="display: none;">
+        x-on:keydown.shift.tab.prevent="prevFocusable().focus()" x-show="show"
+        class="fixed inset-0 z-10 overflow-y-auto" style="display: none;">
         <div class="flex items-end justify-center min-h-screen px-4 pt-4 pb-10 text-center sm:block sm:p-0">
             <div x-show="show" x-on:click="closeModalOnClickAway()" x-transition:enter="ease-out duration-300"
                 x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
@@ -30,15 +32,13 @@
                 x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
                 x-transition:leave="ease-in duration-200"
                 x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-                x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                x-bind:class="modalWidth"
-                class="inline-block w-full align-middle bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:w-full"
+                x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" x-bind:class="modalWidth"
+                class="inline-block w-full align-middle bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle mb-20 sm:w-full"
                 id="modal-container">
                 @forelse($components as $id => $component)
-                    <div x-show.immediate="activeComponent == '{{ $id }}'" x-ref="{{ $id }}"
-                        wire:key="{{ $id }}">
-                        @livewire($component['name'], $component['attributes'], key($id))
-                    </div>
+                <div x-show.immediate="activeComponent == '{{ $id }}'" x-ref="{{ $id }}" wire:key="{{ $id }}">
+                    @livewire($component['name'], $component['attributes'], key($id))
+                </div>
                 @empty
                 @endforelse
             </div>
